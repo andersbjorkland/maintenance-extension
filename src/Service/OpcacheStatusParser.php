1c7037d1590604e5726e5091c1636608056a0135
Anders Björkland <git@andersbjorkland.online>
2021-10-18 22:01:32 +0200

    Add check to see if status is initialized.

--- src/Service/OpcacheStatusParser.php
+++ src/Service/OpcacheStatusParser.php
@@ -16,25 +16,32 @@ class OpcacheStatusParser
         $opcacheStatus = false;
 
         if (function_exists('opcache_get_status')) {
-            $opcacheEnabled = true;
             $opcacheStatusTemp = opcache_get_status();
-            $opcacheStatus["full"] = $opcacheStatusTemp["cache_full"];
-            $opcacheStatus["free_memory"] = $opcacheStatusTemp["memory_usage"]["free_memory"] * 1.25 * (10 ** (-7));
-
-            $timeOfRestart = $opcacheStatusTemp["opcache_statistics"]["last_restart_time"];
-            $opcacheStatus["last_restart_time"] = (new DateTime())->setTimestamp($timeOfRestart)->format("Y-m-d H:i:s");
-
             $opcacheConfigs = opcache_get_configuration();
-            if ($opcacheConfigs) {
-                $opcacheStatus["automatic_validation"] = $opcacheConfigs["directives"]["opcache.validate_timestamps"];
-                $opcacheStatus["max_accelerated_files"] = $opcacheConfigs["directives"]["opcache.max_accelerated_files"];
 
-                if (key_exists("version", $opcacheConfigs)) {
-                    if (key_exists("opcache_product_name", $opcacheConfigs["version"])) {
-                        $opcacheVersion .= $opcacheConfigs["version"]["opcache_product_name"];
-                    }
-                    if (key_exists("version", $opcacheConfigs["version"])) {
-                        $opcacheVersion .= " " . $opcacheConfigs["version"]["version"];
+            if ($opcacheStatusTemp && $opcacheConfigs) {
+                $opcacheEnabled = true;
+                $opcacheStatus["full"] = $opcacheStatusTemp["cache_full"];
+                $opcacheStatus["free_memory"] = $opcacheStatusTemp["memory_usage"]["free_memory"] * 1.25 * (10 ** (-7));
+
+                $timeOfRestart = $opcacheStatusTemp["opcache_statistics"]["last_restart_time"];
+                if ($timeOfRestart) {
+                    $opcacheStatus["last_restart_time"] = (new DateTime())->setTimestamp($timeOfRestart)->format("Y-m-d H:i:s");
+                } else {
+                    $opcacheStatus["last_restart_time"] = "none";
+                }
+
+                if ($opcacheConfigs) {
+                    $opcacheStatus["automatic_validation"] = $opcacheConfigs["directives"]["opcache.validate_timestamps"];
+                    $opcacheStatus["max_accelerated_files"] = $opcacheConfigs["directives"]["opcache.max_accelerated_files"];
+
+                    if (key_exists("version", $opcacheConfigs)) {
+                        if (key_exists("opcache_product_name", $opcacheConfigs["version"])) {
+                            $opcacheVersion = $opcacheConfigs["version"]["opcache_product_name"];
+                        }
+                        if (key_exists("version", $opcacheConfigs["version"])) {
+                            $opcacheVersion .= " " . $opcacheConfigs["version"]["version"];
+                        }
                     }
                 }
             }
